39a2eecd10f29b43f15b1f0123245b7644ae1a1b
Arunkumar2108 <95138039+Arunkumar2108@users.noreply.github.com>
2022-02-07 17:44:35 +0530

    Updated app.js

--- app.js
+++ app.js
@@ -5,11 +5,7 @@ const app = express();
 
 const PORT = process.env.PORT | 4000;
 
-//const client = require('./db');
-
 const router = require('./routes/routes');
-
-//client.connect();
 
 app.listen(PORT,() => {
     console.log('Server running at Port',PORT);
